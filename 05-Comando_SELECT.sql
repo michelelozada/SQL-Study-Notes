29b0a347fc742422bafa492ea6b550823314928d
Michele Lozada <michelelozada1@gmail.com>
2022-04-23 19:47:06 -0300

    Update 05-Comando_SELECT.sql

--- 05-Comando_SELECT.sql
+++ 05-Comando_SELECT.sql
@@ -24,36 +24,36 @@
 	(06, 'Andréa Santos', 'asantos@gmail.com'),
 	(07, 'Silvio Soares', 'ssoares@gmail.com'),
 	(08, 'Daniele Santana', 'danisantana@gmail.com'),
-	(09, 'Luis Henrique Garcia', 'lhgarcia@gmail.com'),
+	(09, 'Luis Garcia', 'lgarcia@gmail.com'),
 	(10, 'Heloísa Bormann', 'hbormann@gmail.com');
     
 	SELECT * FROM tb_alunos;
 	/* Retorna:
-	1	Luana Borba				luanab@gmail.com
-	2	Mariana Fernandes		marifernandes@gmail.com
-	3	Cátia Marcondes			catiamarcondes@gmail.com
-	4	Marcos Góes				marcosgoes@gmail.com
-	5	Sônia de Morais			sonia_morais@gmail.com
-	6	Andréa Santos			asantos@gmail.com
-	7	Silvio Soares			ssoares@gmail.com
-	8	Daniele Santana			danisantana@gmail.com
-	9	Luis Henrique Garcia	lhgarcia@gmail.com
-	10	Heloísa Bormann			hbormann@gmail.com  */
+	1	Luana Borba			luanab@gmail.com
+	2	Mariana Fernandes	marifernandes@gmail.com
+	3	Cátia Marcondes		catiamarcondes@gmail.com
+	4	Marcos Góes			marcosgoes@gmail.com
+	5	Sônia de Morais		sonia_morais@gmail.com
+	6	Andréa Santos		asantos@gmail.com
+	7	Silvio Soares		ssoares@gmail.com
+	8	Daniele Santana		danisantana@gmail.com
+	9	Luis Garcia			lgarcia@gmail.com
+	10	Heloísa Bormann		hbormann@gmail.com  */
     
 	
 -- 2 - A Cláusula ORDER BY:
 	SELECT * FROM tb_alunos ORDER BY nome_aluno; -- ordenação default é em ordem crescente
 	/* Retorna:
-	6	Andréa Santos			asantos@gmail.com
-	3	Cátia Marcondes			catiamarcondes@gmail.com
-	8	Daniele Santana			danisantana@gmail.com
-	10	Heloísa Bormann			hbormann@gmail.com
-	1	Luana Borba				luanab@gmail.com
-	9	Luis Henrique Garcia	lhgarcia@gmail.com
-	4	Marcos Góes				marcosgoes@gmail.com
-	2	Mariana Fernandes		marifernandes@gmail.com
-	7	Silvio Soares			ssoares@gmail.com
-	5	Sônia de Morais			sonia_morais@gmail.com */
+	6	Andréa Santos		asantos@gmail.com
+	3	Cátia Marcondes		catiamarcondes@gmail.com
+	8	Daniele Santana		danisantana@gmail.com
+	10	Heloísa Bormann		hbormann@gmail.com
+	1	Luana Borba			luanab@gmail.com
+	9	Luis Garcia			lgarcia@gmail.com
+	4	Marcos Góes			marcosgoes@gmail.com
+	2	Mariana Fernandes	marifernandes@gmail.com
+	7	Silvio Soares		ssoares@gmail.com
+	5	Sônia de Morais		sonia_morais@gmail.com */
 		
 	SELECT nome_aluno FROM tb_alunos ORDER BY nome_aluno desc;
 	/* Retorna:
@@ -61,7 +61,7 @@
 	Silvio Soares
 	Mariana Fernandes
 	Marcos Góes
-	Luis Henrique Garcia
+	Luis Garcia
 	Luana Borba
 	Heloísa Bormann
 	Daniele Santana
@@ -79,7 +79,7 @@
 	6	Andréa Santos
 	8	Daniele Santana
 	10	Heloísa Bormann
-	9	Luis Henrique Garcia
+	9	Luis Garcia
 	7	Silvio Soares
 	5	Sônia de Morais */
         
@@ -87,21 +87,15 @@
 -- 4. O Operador LIKE:
 	SELECT * from tb_alunos WHERE nome_aluno LIKE '%A'; -- filtra registros presentes na tabela que tem palavras (no campo nome_aluno) que terminam com a letra 'A' 
 	/* Retorna:
-	1	Luana Borba				luanab@gmail.com
-	8	Daniele Santana			danisantana@gmail.com
-	9	Luis Henrique Garcia	lhgarcia@gmail.com */
+	1	Luana Borba		luanab@gmail.com
+	8	Daniele Santana	danisantana@gmail.com
+	9	Luis Garcia		lgarcia@gmail.com */
 			
 	SELECT * from tb_alunos WHERE nome_aluno LIKE 'M%';  -- filtra registros presentes na tabela que tem palavras (no campo nome_aluno) que começam com a letra 'M' 
 	/* Retorna:
 	2	Mariana Fernandes	marifernandes@gmail.com
 	4	Marcos Góes			marcosgoes@gmail.com  */
-    
-    id_aluno | nome_aluno | email_aluno
-	:---: | :---
-	2 |	Mariana Fernandes |	marifernandes@gmail.com
-	4 | Marcos Góes		  | marcosgoes@gmail.com  */
-        
-        
+         
 	SELECT * from tb_alunos WHERE nome_aluno LIKE '%M%'; -- filtra registros presentes na tabela que tem palavras (no campo nome_aluno) que contêm a letra 'M' 
 	/* Retorna:
 	2	Mariana Fernandes	marifernandes@gmail.com
